b941322a6338c5e33a1c44d135c628d1d93f2fd1
Ingon Lee <ingonlee@demoaccountsMBP.hitronhub.home>
2023-07-01 01:32:54 -0700

    update App to fetch game data and load Board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { fetchGameData } from './services/GameService';
+import Board from './ui/Board';
+import { GameData } from './interfaces/Game.interface';
+import styles from './App.module.css';
 
 function App() {
+  const [gameData, setGameData] = useState<GameData>();
+
+  const initGame = async () => {
+    setGameData(undefined);
+    const gameData = await fetchGameData();
+    setGameData(gameData);
+  };
+
+  useEffect(() => {
+    initGame();
+  }, []);
+
   return (
-    <div className="App">
+    <div className={styles.app}>
+      {gameData ? (
+        <>
+          <Board gameData={gameData} resetGame={initGame} />
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
