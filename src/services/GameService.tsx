813a503f69713e4353fb475d5e5b28f96ac2148e
Ingon Lee <ingonlee@demoaccountsMBP.hitronhub.home>
2023-07-01 01:29:20 -0700

    implement service to fetch game data

---
+++ src/services/GameService.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { GameData } from '../interfaces/Game.interface';
+
+const gameServiceApi = axios.create({
+  baseURL: 'http://localhost:9876',
+  validateStatus: (status) => {
+    return status >= 200 && status < 300; // Only resolve for 2xx status codes
+  },
+});
+
+export const fetchGameData = async () => {
+  try {
+    const result = await gameServiceApi.get<GameData>('/init');
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchGameDataWithUserId = async (userId: string) => {
+  try {
+    const result = await gameServiceApi.get<GameData>(`/init/user/${userId}`);
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
